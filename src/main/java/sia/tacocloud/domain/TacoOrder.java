bf4abf4da76a97c2c14fe38bc1e4835556bee298
Praveen Kumar K <praveen.9.kulkarni@gmail.com>
2023-12-03 09:56:09 +0530

    Added a DesignTacoController to handle requests submitted from users while designing their custom tacos and added relevant domain objects and view template

---
+++ src/main/java/sia/tacocloud/domain/TacoOrder.java
@@ -0,0 +1,22 @@
+package sia.tacocloud.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TacoOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        tacos.add(taco);
+    }
+
+}
